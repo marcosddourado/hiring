a0fb36522ee1f46f75afa2517aa890276876021e
Marcos Dourado <marcosdourado92@gmail.com>
2018-01-09 11:14:12 -0200

    adding main.rb

---
+++ main.rb
@@ -0,0 +1,7 @@
+# creates a new destination address, withdraws from the default label to it, gets sent transactions, and the current price
+
+require 'block_io'
+
+# please use the Dogecoin Testnet API key here
+puts BlockIo.set_options :api_key => 'e61b-2513-431a-3906', :pin => 'shakurraph2', :version => 2
+
