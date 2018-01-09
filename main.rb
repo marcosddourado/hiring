66fb61ea87cb7319ddbcfb1530f8bf6533f7c2e4
Marcos Dourado <marcosdourado92@gmail.com>
2018-01-09 11:38:41 -0200

    sending bitcoin testnets

--- main.rb
+++ main.rb
@@ -5,3 +5,5 @@ require 'block_io'
 # please use the Dogecoin Testnet API key here
 puts BlockIo.set_options :api_key => 'e61b-2513-431a-3906', :pin => 'shakurraph2', :version => 2
 
+puts BlockIo.withdraw_from_labels(:from_labels => 'default', :to_address => '2N23YsTMNSdgxcfebqekGcJjDEkqKk8eunm', :amount => '0.001')
+
